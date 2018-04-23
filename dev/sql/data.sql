554844a8ea9c72575664a0a218f22b38bf93cf6b
Alexander Strakh <strakh@ispras.ru>
2018-04-23 13:50:30 +0300

    Update SQL

--- dev/sql/data.sql
+++ dev/sql/data.sql
@@ -1,6 +1,2 @@
-INSERT INTO currencies VALUES(1, "GESE", "Gese");
-INSERT INTO currencies VALUES(2, "GP", "Gese Power");
-INSERT INTO currencies VALUES(3, "GD", "Gese Dollar");
-
 INSERT INTO scheduled_tasks VALUES(1, 0);
 INSERT INTO scheduled_tasks VALUES(2, 0);
